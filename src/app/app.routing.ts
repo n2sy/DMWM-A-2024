8d4ad37d60d323bc90fe89023e2f7225866036a8
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-03-29 13:46:53 +0100

    add children routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -10,16 +10,62 @@ import { UpdateComponent } from './update/update.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const myRoutes: Routes = [
+  //Version 3 avec children
   { path: '', component: AccueilComponent },
-  { path: 'cv', component: CvComponent },
-  { path: 'cv/add', component: AddComponent },
-  { path: 'cv/:id', component: InfosComponent },
-  { path: 'cv/:id/edit', component: UpdateComponent },
+  {
+    path: 'cv',
+    component: CvComponent,
+    children: [
+      //   { path: '', component: CvComponent },
+      { path: 'add', component: AddComponent },
+      {
+        path: ':id',
+        children: [
+          { path: '', component: InfosComponent },
+          { path: 'edit', component: UpdateComponent },
+        ],
+      },
+    ],
+  },
   { path: 'servers', component: ManageServersComponent },
   { path: 'accounts', component: HomeAccountComponent },
   { path: 'ms-word', component: MsWordComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', redirectTo: 'not-found' },
 ];
+
+//Version 2 avec children
+// { path: '', component: AccueilComponent },
+// {
+//   path: 'cv',
+//   children: [
+//     { path: '', component: CvComponent },
+//     { path: 'add', component: AddComponent },
+//     {
+//       path: ':id',
+//       children: [
+//         { path: '', component: InfosComponent },
+//         { path: 'edit', component: UpdateComponent },
+//       ],
+//     },
+//   ],
+// },
+// { path: 'servers', component: ManageServersComponent },
+// { path: 'accounts', component: HomeAccountComponent },
+// { path: 'ms-word', component: MsWordComponent },
+// { path: 'not-found', component: NotFoundComponent },
+// { path: '**', redirectTo: 'not-found' },
+
+// Version 1 sans children
+// { path: '', component: AccueilComponent },
+//   { path: 'cv', component: CvComponent },
+//   { path: 'cv/add', component: AddComponent },
+//   { path: 'cv/:id', component: InfosComponent },
+//   { path: 'cv/:id/edit', component: UpdateComponent },
+//   { path: 'servers', component: ManageServersComponent },
+//   { path: 'accounts', component: HomeAccountComponent },
+//   { path: 'ms-word', component: MsWordComponent },
+//   { path: 'not-found', component: NotFoundComponent },
+//   { path: '**', redirectTo: 'not-found' },
 
 export const DMWM_ROUTING = RouterModule.forRoot(myRoutes);
